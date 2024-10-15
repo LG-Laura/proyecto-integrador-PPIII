42873f3a04e631f07a7d3f6d34ea2882a582c96d
Laura López <98815427+LG-Laura@users.noreply.github.com>
2024-10-15 17:07:41 -0300

    Rama laura (#9)
    * Se agrera un commit de prueba
    
    * Se agrega estructuray sestilos de la landing inicial, barra menu
    
    * Se agrega la estructura para las secciones, sobre mi, habilidades, proyectos y formulario de contacto
    
    * Se agregan estilod css para las secciones, sobre mi, habilidades, proyectos y formulario de contacto
    
    * Se agrega archivo js para mostrar efecto en la barra de navegación al hacer click en menues
    
    * Se agregan recursos de imagenes para la sección de proyectos

---
+++ main/Laura/laura.js
@@ -0,0 +1,15 @@
+particlesJS("particle-canvas", {
+    "particles": {
+        "number": { "value": 80, "density": { "enable": true, "value_area": 800 } },
+        "color": { "value": "#ff6c39" },
+        "shape": { "type": "circle" },
+        "opacity": { "value": 0.6, "random": true },
+        "size": { "value": 7, "random": true },
+        "move": { "speed": 3 }
+    },
+    "interactivity": {
+        "events": {
+            "onhover": { "enable": true, "mode": "repulse" }
+        }
+    }
+});
