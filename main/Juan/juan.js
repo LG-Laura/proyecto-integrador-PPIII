861bdf736bcdad0ff0a2213c3e3327ba5afb7b54
Juan Zagardia <zagardiajuan@gmail.com>
2024-10-14 21:54:23 -0300

    se arreglo el carrousel de tecnologias poero se rompio el mobil

--- main/Juan/juan.js
+++ main/Juan/juan.js
@@ -1,10 +1,19 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const carousel = document.querySelector('.tech-carousel');
-  const totalWidth = carousel.scrollWidth;
-  
-  // Cuando la animación completa el ciclo, restablecemos la posición del carrusel.
-  carousel.addEventListener('animationiteration', () => {
-    carousel.style.transform = 'translateX(0)'; // Reinicia el scroll
-  });
-});
 
+// let currentIndex = 0; // Índice de la imagen actual
+// const items = document.querySelectorAll('.tech-item'); // Selecciona todos los ítems
+
+// function showNextImage() {
+//     items[currentIndex].style.display = 'none'; // Oculta la imagen actual
+//     currentIndex = (currentIndex + 1) % items.length; // Incrementa el índice y reinicia si es necesario
+//     items[currentIndex].style.display = 'flex'; // Muestra la siguiente imagen
+// }
+
+// // Inicialmente oculta todas las imágenes excepto la primera
+// items.forEach((item, index) => {
+//     if (index !== 0) {
+//         item.style.display = 'none';
+//     }
+// });
+
+// // Cambia la imagen cada 3 segundos
+// setInterval(showNextImage, 1500);
