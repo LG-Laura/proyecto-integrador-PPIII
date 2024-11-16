8cf2ae000084d2f0b22883bc0c304988de1ed97d
Javier Duarte <javierduarte0407@gmail.com>
2024-11-16 08:28:46 -0300

    Cambios pantallas
    Subo los cambios para no perderlos. Ya cree las vistas y estan enlazadas de una manera

---
+++ main/Javier/Porfolio/Models/ContactoViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Porfolio.Models
+{
+    public class ContactoViewModel
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Email { get; set; }
+        public string Comentario { get; set; }
+    }
+}
---
+++ main/Javier/Porfolio/Models/ProyectoViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Porfolio.Models
+{
+    public class ProyectoViewModel
+    {
+        public string Titulo { get; set; }
+        public string Descripcion { get; set; }
+        public string ImagenURL { get; set; }
+        public string Link { get; set; }
+
+
+    }
+}
