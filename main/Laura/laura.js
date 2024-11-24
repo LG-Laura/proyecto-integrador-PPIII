17a6826f046905ae981ba4941be7b2fa83ce6397
LG-Laura <lalopezgar@gmail.com>
2024-11-23 23:18:12 -0300

    Se agregó configuracion y envio de email

--- main/Laura/laura.js
+++ main/Laura/laura.js
@@ -1,50 +1,36 @@
-// particlesJS("particle-canvas", {
-//     "particles": {
-//         "number": { "value": 80, "density": { "enable": true, "value_area": 800 } },
-//         "color": { "value": "#ff6c39" },
-//         "shape": { "type": "circle" },
-//         "opacity": { "value": 0.6, "random": true },
-//         "size": { "value": 7, "random": true },
-//         "move": { "speed": 3 }
-//     },
-//     "interactivity": {
-//         "events": {
-//             "onhover": { "enable": true, "mode": "repulse" }
-//         }
-//     }
-// });
 
-// Inicializa EmailJS
-// (function(){
-//     emailjs.init("UnwxxufSfCOzt9nYZ");
-// })();
-document.getElementById('form')
- .addEventListener('submit', function(event) {
-   event.preventDefault();
+document.getElementById('contactForm').addEventListener("submit", function(event){
+  event.preventDefault();
 
-   const button = document.getElementById('button');
-    button.value = 'Enviando...';
-    
-   // Muestra el mensaje de carga
-   document.querySelector(".loading").style.display = "block";
-   document.querySelector(".error-message").style.display = "none";
-   document.querySelector(".sent-message").style.display = "none";
-     
-   const serviceID = 'default_service';
-   const templateID = 'template_kw8az8h';
+  let name = document.getElementById('name').value;
+  let email = document.getElementById('email').value;
+  let message = document.getElementById('message').value;
 
-   emailjs.sendForm(serviceID, templateID, this)
-   .then(function(response) {
-    console.log('SUCCESS!', response.status, response.text);
-        document.querySelector(".loading").style.display = "none";
-        document.querySelector(".sent-message").style.display = "block";
-        document.getElementById("form").reset(); // Limpiar el formulario
-  }, function(error) {
+  let params = {
+    Email: email,
+    Name: name,
+    Message: message
+  };
+
+  // Realiza la petición POST a mi API
+  fetch('http://apinotificaciones.somee.com/api/Email/SendMailLaura', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(params)
+  })
+  .then(function(response) {
+    if (response.ok) {
+        document.getElementById("contactForm").reset();
+    } else {
+        alert('ocurrio un error')
+    }
+  })
+  .catch(function(error) {
     console.log('FAILED...', error);
-        document.querySelector(".loading").style.display = "none";
-        document.querySelector(".error-message").innerText = "Hubo un error al enviar el mensaje. Inténtalo de nuevo más tarde.";
-        document.querySelector(".error-message").style.display = "block";
   });
 });
+
 
   
